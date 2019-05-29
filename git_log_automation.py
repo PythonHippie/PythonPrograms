fb8b1276953c84a2514e2dbcdc55ae26a536f2f9
PythonHippie <42512235+PythonHippie@users.noreply.github.com>
2019-05-29 10:38:45 +0530

    added creating directories script

--- git_log_automation.py
+++ git_log_automation.py
@@ -200,3 +200,46 @@ if __name__ == '__main__':
     #| Aristidis  | Papageorgo | 28  | Senior    |
     #|            | poulos     |     | Reseacher |
     #---------------------------------------------
+    ===================================================
+    import os
+ 
+def main():
+    
+    # Create directory
+    dirName = 'tempDir'
+    
+    try:
+        # Create target Directory
+        os.mkdir(dirName)
+        print("Directory " , dirName ,  " Created ") 
+    except FileExistsError:
+        print("Directory " , dirName ,  " already exists")        
+    
+    # Create target Directory if don't exist
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
+        print("Directory " , dirName ,  " Created ")
+    else:    
+        print("Directory " , dirName ,  " already exists")
+    
+    dirName = 'tempDir2/temp2/temp'
+    
+    # Create target directory & all intermediate directories if don't exists
+    try:
+        os.makedirs(dirName)    
+        print("Directory " , dirName ,  " Created ")
+    except FileExistsError:
+        print("Directory " , dirName ,  " already exists")  
+        
+    
+    # Create target directory & all intermediate directories if don't exists
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+        print("Directory " , dirName ,  " Created ")
+    else:    
+        print("Directory " , dirName ,  " already exists")    
+        
+    
+if __name__ == '__main__':
+    main()
+    =================================
